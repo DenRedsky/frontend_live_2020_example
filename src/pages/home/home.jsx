3a75fef74053f84d618b443cc9f5bdab75831ed9
Denis Krasnovskiy <DIKrasnovskii@domclick.ru>
2020-09-20 23:30:04 +0300

    rebase

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="block">
+    Конференция Frontend Live — это шесть дней увлекательной программы.
+    Frontend Live посвящена всем аспектам разработки клиентской части веб проектов.
+  </div>
+);
+
+export default Home;
